f6a543eea062750880e97fee94874fe0f352a31d
vanshi1405 <vanshi1405shah@gmail.com>
2023-01-27 15:22:44 +0530

    done task1-(1)

--- task1.py
+++ task1.py
@@ -1,31 +1,23 @@
 start=int(input("enter a starting range"))
 end=int(input("enter a ending range"))
-l=[]
-sq=[]
+l=[i for i in range(start,end+1)]
+d1={i:i*i for i in l}
 
-
-for i in range(start,end+1):
-    l.append(i)
-
-for i in l:
-    sq.append(i*i)
-
-
-vi=[]
+vi=set()
 c=0
 
-d1=dict(zip(l,sq))
-for key,val in d1.items():
-    print(key,val)
+
+# d1=dict(zip(l,sq))
+# for key,val in d1.items():
+#     print(key,val)
 
 
 for i in range(0,3):
     if c<3:
-        n=int(input("enter a num to get a square of that number"))
-        if  n in d1.keys() and n  not in vi   :
-            k=d1.get(n)
-            print(k)
-            vi.append(i)
+        n=int(input(f"enter a num to get a square of that number and number is between {start} to {end} "))
+        if  d1.get(n) and n  not in vi   :
+            print(d1.get(n))
+            vi.add(n)
             # print("jj")
 
         elif  n in vi:
